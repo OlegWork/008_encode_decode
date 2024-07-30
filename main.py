2e6efc8bb8e2976795fc5f13127c3cac085ba8f6
Oleg Work <zeus.set@gmail.com>
2024-07-30 22:17:42 +0300

    minor changes

--- main.py
+++ main.py
@@ -7,8 +7,14 @@ while continue_cyphering is True:
     user_message = input("Type your message:\n")
     shift_number = int(input("Type the shift number:\n"))
 
-    encoded_result = "xxxxx"
-    print(f"Here's the encoded result: {encoded_result}")
+    if user_action == "encode":
+        encoded_result = "test encode"
+        print(f"Here's the encoded result: {encoded_result}\n")
+    elif user_action == "decode":
+        decode_result = "test decode"
+        print(print(f"Here's the decoded result: {decode_result}\n"))
+    else:
+        print("Wrong value!")
 
     continue_working = input("Type 'yes' if you want to go again. Otherwise type 'no'.").lower()
     if continue_working == "no":
@@ -16,4 +22,4 @@ while continue_cyphering is True:
 
 
 else:
-    print("Goodbye")
+    print("Goodbye\n")
